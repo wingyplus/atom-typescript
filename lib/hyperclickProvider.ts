7e3f7cc5a6fc14dff989f45e233742c449501c56
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-07-06 19:20:52 +0700

    Enable hyperclick only .ts and .tsx grammar
    Ref #448

--- lib/hyperclickProvider.ts
+++ lib/hyperclickProvider.ts
@@ -1,6 +1,8 @@
 import * as parent from "./worker/parent";
 import * as atomUtils from "./main/atom/atomUtils";
+import {Set} from "immutable";
 
+const TS_GRAMMARS = Set<string>(["source.ts", "source.tsx"]);
 
 export let providerName = "typescript-hyperclick-provider";
 
@@ -8,6 +10,10 @@ export function getSuggestionForWord(textEditor: AtomCore.IEditor, text: string,
     return {
         range: range,
         callback() {
+            if (!TS_GRAMMARS.has(textEditor.getGrammar().scopeName)) {
+                return null;
+            }
+
             let filePathPosition = {
               filePath: textEditor.getPath(),
               position: atomUtils.getEditorPositionForBufferPosition(textEditor, range.start)
